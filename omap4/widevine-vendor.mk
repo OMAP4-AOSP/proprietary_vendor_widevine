657d3acdcc3582420281d4a84e331d225f900724
D. Andrei Măceș <dmaces@nd.edu>
2016-06-10 12:04:44 -0400

    omap4: Convert blob copying to modules w/ strip & reloc

--- omap4/widevine-vendor.mk
+++ omap4/widevine-vendor.mk
@@ -1,1 +1,9 @@
-$(call inherit-product, vendor/widevine/omap4/widevine-vendor-blobs.mk)
+$(call inherit-product-if-exists, vendor/widevine/omap4/widevine-vendor-blobs.mk)
+
+PRODUCT_PACKAGES += \
+	libdrmwvmplugin \
+	libwvdrmengine \
+	libdrmmtkutil \
+	libwvdrm_L3 \
+	libwvm \
+	libWVStreamControlAPI_L3
